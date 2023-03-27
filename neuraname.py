2fa942804ad2658f58d7200a5c607b431e22a951
iiam33 <hongyoong.yeoh@gmail.com>
2023-03-27 19:45:01 +0800

    able to create plot from the bigram counts

---
+++ neuraname.py
@@ -0,0 +1,40 @@
+import torch
+import matplotlib.pyplot as plt
+
+names = open('names.txt', 'r').read().splitlines()
+
+bigram = {}
+
+chars = sorted(list(set(''.join(names))))
+stoi = {s: i+1 for i, s in enumerate(chars)}
+stoi["."] = 0
+
+N = torch.zeros((27, 27), dtype=torch.int32)
+
+for name in names:
+    chars = ["."] + list(name) + ["."]
+    for ch1, ch2 in zip(chars, chars[1:]):
+        ix1 = stoi[ch1]
+        ix2 = stoi[ch2]
+
+        N[ix1, ix2] += 1
+
+itos = {i: s for s, i in stoi.items()}
+
+plt.figure(figsize=(20, 20))
+plt.imshow(N, cmap="Blues")
+
+for i in range(27):
+    for j in range(27):
+        chstr = itos[i]+itos[j]
+        plt.text(j, i, chstr, ha="center", va="bottom", color="grey")
+        plt.text(j, i, N[i, j].item(), ha="center", va="top", color="grey")
+
+plt.axis("off")
+plt.show()
+
+# print(N)
+# plt.imshow(N)
+# plt.show()
+
+# print(sorted(bigram.items(), key=lambda b: -b[1]))
