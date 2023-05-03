bdbc6373370fcec1a7622762552e9d7ff6c156e4
iiam33 <hongyoong.yeoh@gmail.com>
2023-05-03 11:44:54 +0800

    created softmax activation function and able to calculate loss

--- neuraname.py
+++ neuraname.py
@@ -27,3 +27,41 @@ for name in names[:5]:
 
 X = torch.tensor(X)
 Y = torch.tensor(Y)
+
+g = torch.Generator().manual_seed(2147483647)
+C = torch.randn((27, 2), generator=g)
+# embedding
+emb = C[X]
+
+# # first approach of concatenating the embeddings
+# torch.cat(torch.unbind(emb, 1), 1)
+
+# # second approach of concatenating the embeddings
+# # more efficient and will not create redundant memory
+# emb.view(32, 6)
+
+W1 = torch.randn((6, 100), generator=g)
+b1 = torch.randn(100, generator=g)
+
+# pytorch will infer the -1 should be
+h = torch.tanh(emb.view(-1, 6) @ W1 + b1)
+
+W2 = torch.randn((100, 27), generator=g)
+b2 = torch.randn(27, generator=g)
+
+parameters = [C, W1, b1, W2, b2]
+# number of parameters in total
+print("parameters:", sum(p.nelement() for p in parameters))
+
+logits = h @ W2 + b2
+# # softmax activation function / classification
+# # this is a manual classification function
+# counts = logits.exp()
+# prob = counts / counts.sum(1, keepdim=True)
+# loss = -prob[torch.arange(32), Y].log().mean()
+
+# this is a built-in classification function from pytorch
+# which results the same output as softmax activation function
+loss = F.cross_entropy(logits, Y)
+
+print("loss:", loss)
